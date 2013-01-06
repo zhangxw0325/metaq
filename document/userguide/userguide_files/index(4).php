7d7299f75d5871c47edb4bf250d68bd15807a330
Wangxr <vintage.wang@gmail.com>
2013-01-06 10:53:42 +0800

    add user guide

---
+++ document/userguide/userguide_files/index(4).php
@@ -0,0 +1,9 @@
+/* generated javascript */
+var skin = 'modern';
+var stylepath = '/skins';
+
+/* MediaWiki:Common.js */
+/* 此处的JavaScript将加载于所有用户每一个页面。 */
+
+/* MediaWiki:Modern.js */
+/* 此处的JavaScript将加载于使用Modern皮肤的用户 */
